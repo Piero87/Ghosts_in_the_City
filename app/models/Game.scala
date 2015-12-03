77624d7faf25d5b927a66ee22dc07dc2d6428651
Pietro Nompleggio <Piero@MacBook-Pro-di-Pietro.local>
2015-12-03 12:41:41 +0100

    Initial commit

---
+++ app/models/Game.scala
@@ -0,0 +1,14 @@
+package models
+
+import org.joda.time.DateTime
+import play.api.libs.json.{ Json, Format }
+import play.api.db.slick.DatabaseConfigProvider
+import slick.driver.JdbcProfile
+import play.api.Play.current
+import play.api.db.DBApi
+import scala.concurrent.Future
+import play.api.libs.concurrent.Execution.Implicits._
+
+class Game {
+  
+}
