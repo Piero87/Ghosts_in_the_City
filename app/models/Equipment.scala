15f1b686a85b7651316fcfaabeaed6bfd8b8e2c9
Richiard Casadei <shadow674@libero.it>
2015-12-04 13:02:24 +0100

    Equipment and Ghost model done

--- app/models/Equipment.scala
+++ app/models/Equipment.scala
@@ -1,5 +1,65 @@
 package models
 
-class Equipment {
-  
+import org.joda.time.DateTime
+import play.api.libs.json.{ Json, Format }
+
+import play.api.db.slick.DatabaseConfigProvider
+import slick.driver.JdbcProfile
+import play.api.Play.current
+import play.api.db.DBApi
+import scala.concurrent.Future
+import play.api.libs.concurrent.Execution.Implicits._
+
+import SlickMapping.jodaDateTimeMapping
+
+case class Equipment (
+  id: Option[Long],
+  player_id: Int,
+  item_id: Int
+)
+
+object Equipment {
+  implicit val format: Format[Equipment] = Json.format[Equipment]
+
+  protected val dbConfig = DatabaseConfigProvider.get[JdbcProfile](current)
+  import dbConfig._
+  import dbConfig.driver.api._
+
+  class EquipmentsTable(tag: Tag) extends Table[Equipment](tag, "EQUIPMENTS") {
+
+    def id = column[Long]("ID", O.PrimaryKey, O.AutoInc)
+    def player_id = foreignKey("PLAYER", id, Player.table)(_.id)
+    def item_id = foreignKey("ITEM", id, Item.table)(_.id)
+
+
+    def * = (id.?, player_id, item_id) <>
+      ((Equipment.apply _).tupled, Equipment.unapply)
+  }
+
+  val table = TableQuery[EquipmentsTable]
+
+  def list: Future[Seq[Equipment]] = {
+    val equipmentList = table.result
+    db.run(equipmentList)
+  }
+
+  def getByID(equipmentID: Long): Future[Option[Equipment]] = {
+    val equipmentByID = table.filter { f =>
+      f.id === equipmentID
+    }.result.headOption
+
+    db.run(equipmentByID)
+  }
+
+  def create(newEquipment: Equipment): Future[Equipment] = {
+    val insertion = (table returning table.map(_.id)) += newEquipment
+
+    val insertedIDFuture = db.run(insertion)
+
+    val createdCopy: Future[Equipment] = insertedIDFuture.map { resultID =>
+      newEquipment.copy(id = Option(resultID))
+    }
+
+    createdCopy
+  }
 }
--- app/models/Ghost.scala
+++ app/models/Ghost.scala
@@ -1,5 +1,73 @@
 package models
 
-class Ghost {
-  
+import org.joda.time.DateTime
+import play.api.libs.json.{ Json, Format }
+
+import play.api.db.slick.DatabaseConfigProvider
+import slick.driver.JdbcProfile
+import play.api.Play.current
+import play.api.db.DBApi
+import scala.concurrent.Future
+import play.api.libs.concurrent.Execution.Implicits._
+
+import SlickMapping.jodaDateTimeMapping
+
+case class Ghost (
+  id: Option[Long],
+  player_id: Long,
+  item_id: Long,
+  status: Int,
+  latitude: Float,
+  longitude: Float,
+  treasure_id: Long
+)
+
+object Ghost {
+  implicit val format: Format[Ghost] = Json.format[Ghost]
+
+  protected val dbConfig = DatabaseConfigProvider.get[JdbcProfile](current)
+  import dbConfig._
+  import dbConfig.driver.api._
+
+  class GhostsTable(tag: Tag) extends Table[Ghost](tag, "GHOSTS") {
+
+    def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
+    def player_id = column[Int]("PLAYER_ID")
+    def item_ide = foreignKey("ITEM", id, Item.table)(_.id)
+    def status = column[Int]("STATUS")
+    def latitude = column[Float]("LATITUDE")
+    def longitude = column[Float]("LONGITUDE")
+    def treasure_id = foreignKey("TREASURE", id, Treasure.table)(_.id)
+
+
+    def * = (id.?, player_id, treasure_id) <>
+      ((Ghost.apply _).tupled, Ghost.unapply)
+  }
+
+  val table = TableQuery[GhostsTable]
+
+  def list: Future[Seq[Ghost]] = {
+    val ghostList = table.result
+    db.run(ghostList)
+  }
+
+  def getByID(ghostID: Int): Future[Option[Ghost]] = {
+    val ghostByID = table.filter { f =>
+      f.id === ghostID
+    }.result.headOption
+
+    db.run(ghostByID)
+  }
+
+  def create(newEquipment: Ghost): Future[Ghost] = {
+    val insertion = (table returning table.map(_.id)) += newEquipment
+
+    val insertedIDFuture = db.run(insertion)
+
+    val createdCopy: Future[Ghost] = insertedIDFuture.map { resultID =>
+      newEquipment.copy(id = Option(resultID))
+    }
+
+    createdCopy
+  }
 }
