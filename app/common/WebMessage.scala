50022beefde18f77333f342663fba7a7db9b625e
andrea buscarini <andrea.buscarini@studio.unibo.it>
2015-12-17 11:00:55 +0100

    new webmessages: game gameslist

--- app/common/WebMessage.scala
+++ app/common/WebMessage.scala
@@ -5,6 +5,9 @@ import play.api.mvc.WebSocket.FrameFormatter
 
 case class NewGame(name: String)
 
+case class Game(name: String, id: Long)
+
+case class GamesList(gameList: Seq[Game])
 
 /**
  * Contains all of the web socket messages and their json formats
@@ -22,9 +25,30 @@ object WebMessage {
   implicit val newGameFormat = Json.format[_NewGame]
   
   /**
+   * Sent by the client with the game's info
+   */
+  case class _Game(
+    name: String,
+    id: Long
+  )extends WebMessage
+  implicit val gameFormat = Json.format[_Game]
+  
+  /**
+   * Sent by the client with the game's list
+   */
+  case class _GamesList(
+    gamesList: Seq[_Game]
+  )extends WebMessage
+  implicit val gamesListFormat = Json.format[_GamesList]
+  
+  
+  /**
    * Converts json into WebSocketMessages
    */
-  implicit val reads: Reads[WebMessage] = (JsPath \ "new_game").read[_NewGame].map(identity[WebMessage])
+  implicit val reads: Reads[WebMessage] = 
+    (JsPath \ "new_game").read[_NewGame].map(identity[WebMessage]) orElse 
+    (JsPath \ "game").read[_Game].map(identity[WebMessage]) orElse
+    (JsPath \ "games_list").read[_GamesList].map(identity[WebMessage])
     
   /**
    * Converts WebSocketMessages into Json
@@ -32,6 +56,8 @@ object WebMessage {
   implicit object writes extends Writes[WebMessage] {
     override def writes(o: WebMessage): JsValue = o match {
       case ng: _NewGame       => (JsPath \ "new_game").write(newGameFormat).writes(ng)
+      case g:  _Game          => (JsPath \ "game").write(gameFormat).writes(g)
+      case gl:  _GamesList          => (JsPath \ "games_list").write(gamesListFormat).writes(gl)
     }
   }
   
