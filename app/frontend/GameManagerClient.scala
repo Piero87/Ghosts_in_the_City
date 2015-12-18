68e6377fea3231ce807d2352bc9ebadc28776d50
Piero87 <pietro.nompleggio@gmail.com>
2015-12-18 16:32:00 +0100

    Update GameManagerClient.scala

--- app/frontend/GameManagerClient.scala
+++ app/frontend/GameManagerClient.scala
@@ -8,10 +8,6 @@ import scala.util.{Failure, Success}
 import play.api.Logger
 import common._
 
-/*
- * JSONNNNNNNN
- */
-
 object GameManagerClient {
   
   def props(backend: ActorRef) = Props(new GameManagerClient(backend))
@@ -23,20 +19,24 @@ class GameManagerClient (backend: ActorRef) extends Actor {
   var gameManagerBackend: ActorRef = _
   var clientConnection: ActorRef = _
   var game_name = ""
+  var game_id = java.util.UUID.randomUUID.toString
+  var game_n_players = 0
   
   def receive = {
     case NewGame(name,n_players) =>
       Logger.info("GameManagerClient: NewGame request")
       game_name = name
+      game_n_players = n_players
       clientConnection = sender()
       implicit val timeout = Timeout(5 seconds)
       implicit val ec = context.dispatcher
       val future = backend ? NewGame(name,n_players)
       future.onSuccess { 
-        case Game(id,name,n_players) => 
+        case Game(id,name,n_players, status) => 
           Logger.info ("GameManagerClient: Backend Game Manager path: "+sender.path)
+          game_id = id
           gameManagerBackend = sender
-          clientConnection ! Game(id,name,n_players)
+          clientConnection ! Game(id,name,n_players, status)
       }
       future onFailure {
         case e: Exception => Logger.info("****** ERRORE ******")
