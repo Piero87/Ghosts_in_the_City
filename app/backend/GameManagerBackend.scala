164b68ed0107fb86307d5210ecb912f713f87817
Piero87 <pietro.nompleggio@gmail.com>
2015-12-18 16:29:44 +0100

    Update GameManagerBackend.scala

--- app/backend/GameManagerBackend.scala
+++ app/backend/GameManagerBackend.scala
@@ -10,14 +10,24 @@ class GameManagerBackend () extends Actor {
   var game_name = ""
   var game_id = java.util.UUID.randomUUID.toString
   var game_n_players = 0
+  var game_status = 0
+  
+  //ENUM GAME STATUS
+  // 0 = waiting_players
+  // 1 = started
+  // 2 = paused
+  // 3 = finished
   
   def receive = {
     case NewGame(name,n_players) =>
       Logger.info("GameManagerBackend: NewGame request")
       game_name = name
       game_n_players = n_players
+      game_status = 0
       gameManagerClient = sender()
-      gameManagerClient ! Game(game_id,name,n_players)
+      gameManagerClient ! Game(game_id,name,n_players,game_status)
+    case GameStatus =>
+      sender() ! Game(game_id,game_name,game_n_players,game_status)
   }
   
   def newGame () = {
