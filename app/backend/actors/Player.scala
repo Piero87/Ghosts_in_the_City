d2a61e1e6e63ee3fe0a225a9452d8067cb46a88e
Pietro Nompleggio <pietro.nompleggio@gmail.com>
2016-01-08 15:47:27 +0100

    remove database and change project name

--- app/backend/actors/Player.scala
+++ app/backend/actors/Player.scala
@@ -4,37 +4,22 @@ import akka.actor.{ ActorRef, Actor }
 import akka.actor.Props
 import play.api.Logger
 import play.api.libs.json._
-import play.extras.geojson.Point
-import play.extras.geojson.Polygon
-import play.extras.geojson.LatLng
-
+import common._
 
 object Player {
-  
-  /**
-   * UpdatePlayerPosition Events from the client side
-   */
-  case class UpdatePlayerPosition(position: Point[LatLng])
 
   /**
    * Actor Props 
    */
-  def props(area: Polygon[LatLng], starting_position: Point[LatLng], id: Long): Props = Props(new Player(area, starting_position, id))
+  def props(area : Polygon, position: Point): Props = Props(new Player(area, position))
   
 }
 
-class Player(area: Polygon[LatLng], starting_position: Point[LatLng], id: Long) extends Actor{
-  
-  import Player._
-  
-  var pos: Point[LatLng] = starting_position
+class Player(area : Polygon, position: Point) extends Actor{
   
   def receive = {
-   case UpdatePlayerPosition(position) => 
-      Logger.info("Updated position received")
-      pos = position
-      // Qui farò qualcosa con la nuova posizione o me ne sbatto dopo averla salvata? 
-      sender ! "Done"
+   case "test" => 
+      Logger.info("Test")
   }
   
 }
