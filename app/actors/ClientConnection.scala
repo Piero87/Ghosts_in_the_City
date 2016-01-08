cf91b5fab3db937c79656db03131b10fd59d73ae
Marco Zaccheroni <marco.zaccheroni2@studio.unibo.it>
2016-01-08 18:31:01 +0100

    ...

--- app/actors/ClientConnection.scala
+++ app/actors/ClientConnection.scala
@@ -34,7 +34,8 @@ class ClientConnection(username: String, uid: String, upstream: ActorRef,fronten
           newGameResult match {
             case s: JsSuccess[NewGameJSON] => 
               var user_info = new UserInfo(uid,username,team,Point(0,0))
-              val future = frontendManager ? NewGame(s.get.name.replaceAll(" ", "_") + "_" + System.currentTimeMillis(),s.get.n_players,user_info,self)
+              var game_name = "\"" + s.get.name + "\" (" + System.currentTimeMillis() + ")"
+              val future = frontendManager ? NewGame(game_name,s.get.n_players,user_info,self)
               future.onSuccess {
                 case GameHandler(game,ref) => 
                   if (ref != null) gameManagerClient = ref
