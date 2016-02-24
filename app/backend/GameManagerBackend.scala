d8f67671eed1e1745663233bda00ecac5baaadee
Pietro Nompleggio <pietro.nompleggio@gmail.com>
2016-02-24 12:08:53 +0100

    fix

--- app/backend/GameManagerBackend.scala
+++ app/backend/GameManagerBackend.scala
@@ -336,7 +336,7 @@ class GameManagerBackend () extends Actor {
       var ghost_id = randomString(8)
       var p_g = new Point (position_ghosts(i).x,position_ghosts(i).y)
       val g_level = rnd.nextInt(2)+1
-      val ghost = context.actorOf(Props(new Ghost(ghost_id,polygon,p_g,g_level,treasure,pos_t)), name = ghost_id)
+      val ghost = context.actorOf(Props(new Ghost(ghost_id,polygon,p_g,g_level,treasure,pos_t,treasure_id)), name = ghost_id)
       var ghost_info = new GhostInfo(ghost_id,g_level,GhostMood.CALM,p_g)
       ghosts = ghosts :+ Tuple2(ghost_info,ghost)
       
@@ -351,7 +351,7 @@ class GameManagerBackend () extends Actor {
       var free_ghost_id = randomString(8)
       var free_p_g = new Point (free_position_ghosts(i).x,free_position_ghosts(i).y)
       val n_treasure = rnd.nextInt(treasures.size)
-      val free_ghost = context.actorOf(Props(new Ghost(free_ghost_id,polygon,free_p_g,3,treasures(n_treasure)._2,treasures(n_treasure)._1.pos)), name = free_ghost_id)
+      val free_ghost = context.actorOf(Props(new Ghost(free_ghost_id,polygon,free_p_g,3,treasures(n_treasure)._2,treasures(n_treasure)._1.pos,treasures(n_treasure)._1.uid)), name = free_ghost_id)
       var free_ghost_info = new GhostInfo(free_ghost_id,3,GhostMood.CALM,free_p_g)
       ghosts = ghosts :+ Tuple2(free_ghost_info,free_ghost)
     }
