8c08a002ab96a6d575593946e0a335d217839ac3
Marco Zaccheroni <marco.zaccheroni2@studio.unibo.it>
2016-03-04 18:12:53 +0100

    ...

--- app/backend/GameManagerBackend.scala
+++ app/backend/GameManagerBackend.scala
@@ -138,12 +138,14 @@ class GameManagerBackend () extends Actor {
     case UpdatePosition(player) =>
       var player_index = (players.zipWithIndex.collect{case (g , i) if(g._1.uid == player.uid) => i}).head
       var u_tmp = players(player_index)._1
+      if (!canvas.contains(player.pos)) {
+        gameManagerClient ! MessageCode(player.uid,MsgCodes.OUT_OF_AREA,"")
+      } else if (!canvas.contains(u_tmp.pos)) {
+        gameManagerClient ! MessageCode(player.uid,MsgCodes.BACK_IN_AREA,"")
+      }
       var player_info = new PlayerInfo(u_tmp.uid,u_tmp.name,u_tmp.p_type,u_tmp.team,player.pos,u_tmp.gold,u_tmp.keys)
       players(player_index) = players(player_index).copy(_1 = player_info)
       sender ! BroadcastUpdatePosition(player)
-      if (!canvas.contains(player.pos)) {
-        gameManagerClient ! MessageCode(player.uid,MsgCodes.OUT_OF_AREA,"")
-      }
     case UpdateGhostsPositions =>
       //Logger.info("UpdateGhostsPositionsBroadcast")
       val tmp_g = ghosts.map(x => x._1)
